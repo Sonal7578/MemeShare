3438a70b3d92e2985b560e986420dafb1e640d5b
Sonal <sonal.dhage26@gmail.com>
2024-05-10 19:15:38 +0200

    Added functions for the respective buttons

--- app/src/main/java/com/example/memeshare/MainActivity.kt
+++ app/src/main/java/com/example/memeshare/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.memeshare
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,4 +18,7 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+
+    fun shareMeme(view: View) {}
+    fun nextMeme(view: View) {}
 }
