5f8cbe5a886aeba2ae975846a401c4a9ecab5041
Sonal <sonal.dhage26@gmail.com>
2024-05-11 10:40:30 +0200

    added dependency for the API calls

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("com.google.android.gms:play-services-cronet:18.0.1")
+
 }
