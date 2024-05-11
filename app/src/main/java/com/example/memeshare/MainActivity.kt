439c98a6b6ecc20c35dc79c7b9de8313b7c2c0b2
Sonal <sonal.dhage26@gmail.com>
2024-05-11 11:15:23 +0200

    Created the loadMeme function and added the url(volley) in it and requested a JSON object response from the provided url

--- app/src/main/java/com/example/memeshare/MainActivity.kt
+++ app/src/main/java/com/example/memeshare/MainActivity.kt
@@ -6,6 +6,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +24,26 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+
+    private fun loadMeme(){
+        // Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://meme-api.com/gimme"
+
+        // Request a JSON object response from the provided URL.
+        val JsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            Response.Listener<JSONObject> { response ->
+                // Handle the JSON response here
+            },
+            Response.ErrorListener {
+                // Handle errors here
+            })
+
+        // Add the request to the RequestQueue.
+        queue.add(JsonObjectRequest)
+    }
+
 
     fun shareMeme(view: View) {}
     fun nextMeme(view: View) {}
