db6f5a1647c4edb187a42749c3590405d0febe27
Sonal <sonal.dhage26@gmail.com>
2024-05-11 12:06:42 +0200

    Added the Glide library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,5 +48,6 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 
     implementation("com.google.android.gms:play-services-cronet:18.0.1")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 
 }
